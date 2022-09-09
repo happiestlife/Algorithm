2a747fbca8554bd9e9995aa5cf57d0e8ce6b848d
happiestlife <75258422+happiestlife@users.noreply.github.com>
2022-09-09 11:14:05 +0900

    [Gold V] Title: AC, Time: 724 ms, Memory: 68452 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String inst = br.readLine();
+
+            int n = Integer.parseInt(br.readLine());
+            boolean forward = true;
+            Deque<String> arr = new ArrayDeque<>();
+            String data = br.readLine();
+            data = data.substring(1, data.length()-1);
+            StringTokenizer stk = new StringTokenizer(data, ",");
+            for(int j = 0; j < n; j++)
+                arr.add(stk.nextToken());
+
+            boolean isFail = false;
+            for(int j = 0; j < inst.length(); j++) {
+                char cmd = inst.charAt(j);
+                if (cmd == 'R'){
+                    if(forward) forward = false;
+                    else forward = true;
+                }
+                else{
+                    if(arr.isEmpty()){
+                        isFail = true;
+                        break;
+                    }
+                    if(forward) arr.poll();
+                    else arr.pollLast();
+                }
+            }
+
+            if(isFail) bw.write("error\n");
+            else{
+                bw.write("[");
+                while(!arr.isEmpty()){
+                   if(forward)bw.write(arr.pollFirst());
+                   else bw.write(arr.pollLast());
+                   if(arr.isEmpty()) break;
+                    bw.write(",");
+                };
+                bw.write("]\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
