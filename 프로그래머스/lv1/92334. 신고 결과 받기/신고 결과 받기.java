02d3c7e3cf88d01dd2b12d40c9950a159a4ae0c1
happiestlife <75258422+happiestlife@users.noreply.github.com>
2023-01-31 20:41:53 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -4,9 +4,11 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int userLen = id_list.length;
+        // key : 신고당한 유저 아이디, value : 해당 유저를 신고한 아이디 목록
+        //                                  (중복이 가능하기 때문에 쉬운 구현을 위한 set 사용) 
         HashMap<String, HashSet<Integer>> reportId = new HashMap<>();
 
-        int[] rs = new int[userLen];
+        // key : 유저 아이디, value : 유저의 번호
         HashMap<String, Integer> rsInd = new HashMap<>();
         for (int i = 0; i < userLen; i++) {
             rsInd.put(id_list[i], i);
@@ -19,15 +21,20 @@ class Solution {
             String reportedUser = stk.nextToken();
 
             HashSet<Integer> rptUsers = reportId.get(reportedUser);
+            // 해당 유저를 이미 신고했다면 아무것도 진행하지 않음
             if (rptUsers != null && rptUsers.contains(rsInd.get(reportUser))) {
                 continue;
             }else{
+                // 해당 유저를 신고한 목록에 신고를 진행한 유저의 아이디 넣기
                 if(rptUsers == null)
                     reportId.put(reportedUser, new HashSet<>());
                 reportId.get(reportedUser).add(rsInd.get(reportUser));
             }
         }
 
+        // 각 유저가 신고당한 횟수를 조사
+        // k번 이상이면 문자 발송
+        int[] rs = new int[userLen];
         for (String s : reportId.keySet()) {
             if (reportId.get(s).size() >= k) {
                 for (Object rptUser : reportId.get(s).toArray()) {
