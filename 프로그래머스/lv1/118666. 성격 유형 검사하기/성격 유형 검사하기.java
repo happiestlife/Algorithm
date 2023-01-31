4cc3c99860517263b069f6f0bc0570cad397dd5d
happiestlife <75258422+happiestlife@users.noreply.github.com>
2023-01-31 20:27:58 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.42 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,21 +1,24 @@
-import java.io.IOException;
-
 class Solution {
     
     static final char[][] character = {{'R', 'T'}, {'C', 'F'}, {'J', 'M'}, {'A', 'N'}};
     static final int[] score = {-1, 3, 2, 1, 0, 1, 2, 3};
     
     public String solution(String[] survey, int[] choices) {
-        int rs[][] = new int[4][2];
+        // 앞[] : 지표, 뒤[] : 타입
+        int rs[][] = new int[4][2];   
 
         for (int i = 0; i < survey.length; i++) {
+            // 어떤 지표인지 인덱스를 찾기
             int posX = findPos(survey[i]);
+            
+            // 계산하기 쉽게 항상 지표의 타입이 사전순으로 정렬되어 있게끔 함 (인덱스롤 통해) 
             int posY = 0;
             if (survey[i].charAt(0) > survey[i].charAt(1)) {
                 posY = 1;
             }
 
             int scr = score[choices[i]];
+            // 점수가 현 지표 타입의 반대 타입에 줘야 하기 때문에 인덱스 바꾸기
             if (choices[i] > 4) {
                 if(posY == 0)
                     posY = 1;
@@ -38,7 +41,7 @@ class Solution {
         return sb.toString();
     }
     
-        public static int findPos(String srvy) {
+    public static int findPos(String srvy) {
         char c = srvy.charAt(0);
         if (c == character[0][0] || c == character[0][1]) {
             return 0;
