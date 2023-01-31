df89e85054f20e9b8e4153d0b4cf01837f669593
happiestlife <75258422+happiestlife@users.noreply.github.com>
2023-01-31 20:07:20 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.39 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,5 +1,6 @@
 class Solution {
     public String solution(int[] numbers, String hand) {
+        // 핸드폰의 좌표 구하기
         int[][] phone = new int[10][2];
         int row = 0, col = 0;
         for (int i = 1; i < phone.length; i++) {
@@ -14,13 +15,18 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int[] lIdx = {3, 0}, rIdx = {3, 2};
         for (int i = 0; i < numbers.length; i++) {
+            // 왼쪽라인인 경우, 왼손 엄지만 사용
             if(numbers[i] % 3 == 1){
                 sb.append("L");
                 lIdx = phone[numbers[i]];
-            } else if ( numbers[i] != 0 && numbers[i] % 3 == 0) {
+            } 
+            // 오른쪽라인인 경우, 오른손 엄지만 사용
+            else if ( numbers[i] != 0 && numbers[i] % 3 == 0) {
                 sb.append("R");
                 rIdx = phone[numbers[i]];
-            }else {
+            }
+            // 중간에 위치 
+            else {
                 int[] numberPoint = phone[numbers[i]];
                 int lDist = calc(numberPoint, lIdx);
                 int rDist = calc(numberPoint, rIdx);
@@ -49,6 +55,7 @@ class Solution {
         return sb.toString();
     }
 
+    // 상하좌우로만 움직일 수 있다는 점을 감안하여 이동거리 계산
     public static int calc(int[] p1, int[] p2) {
         int x = Math.abs(p1[0] - p2[0]);
         int y = Math.abs(p1[1] - p2[1]);
