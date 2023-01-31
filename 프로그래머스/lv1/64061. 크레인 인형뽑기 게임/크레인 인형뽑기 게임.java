1e8e2edd74e9f01afdb9745ae44c549e073b3252
happiestlife <75258422+happiestlife@users.noreply.github.com>
2023-01-31 20:09:34 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.96 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -2,6 +2,8 @@ import java.util.Stack;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
+        // board의 첫번째 []에서 인덱스의 오름차순에 대해 보드의 윗부분부터 시작하기 때문에 
+        // stack에 거꾸로 넣기 위해선 끝에서부터 넣기
         Stack<Integer>[] b = new Stack[board.length];
         for (int i = b.length - 1; i >= 0; i--) {
             for (int j = 0; j < board[i].length; j++) {
@@ -16,6 +18,7 @@ class Solution {
         int rs = 0;
         Stack<Integer> bucket = new Stack<>();
         for (int i = 0; i < moves.length; i++) {
+            // 해당 라인에 아무것도 없으면 바구니에 아무것도 담지 못한다. 
             if(b[moves[i]-1].isEmpty())
                 continue;
 
