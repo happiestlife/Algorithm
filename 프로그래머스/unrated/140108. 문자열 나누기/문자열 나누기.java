1a61c7c17300652a6e004e62ba7b3625046cb5fb
happiestlife <75258422+happiestlife@users.noreply.github.com>
2023-01-31 20:29:52 +0900

    [unrated] Title: 문자열 나누기, Time: 4.52 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -1,10 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.util.*;
 
 class Solution {
     public int solution(String s) {
         int count = 0;
+        // 같은 문자끼리, 다른 문자끼리 스택 / 큐에 넣어놓고 
+        // 문자열을 검사해가면서 검사를 진행하다가 2개의 스택 / 큐의 길이가 같아지면 문자열 분리
+        // 문자열 분리는 2개의 스택 / 큐를 초기화함으로 이루어진다. 
         Stack<Character> same = new Stack<>();
         Stack<Character> diff = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
