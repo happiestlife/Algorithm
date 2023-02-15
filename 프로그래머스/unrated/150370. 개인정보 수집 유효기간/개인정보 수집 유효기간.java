96c9c3e06d032203a001e9eebce639a701298a33
happiestlife <75258422+happiestlife@users.noreply.github.com>
2023-02-15 21:48:10 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 4.13 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -8,9 +8,9 @@ class Solution {
     static final int MONTH_TOTAL_COUNT = 12;
     
     public int[] solution(String today, String[] terms, String[] privacies) {
-        HashMap<Character, Integer> validDate = new HashMap<>();
-
         StringTokenizer stk;
+        // key : 타입, value : 유효기간
+        HashMap<Character, Integer> validDate = new HashMap<>();
         for (int i = 0; i < terms.length; i++) {
             stk = new StringTokenizer(terms[i]);
 
@@ -24,19 +24,23 @@ class Solution {
         for (int i = 0; i < privacies.length; i++) {
             stk = new StringTokenizer(privacies[i]);
 
+            // 개인정보 약관의 시작일과 타입
             String strtDT = stk.nextToken();
             char type = stk.nextToken().charAt(0);
 
             int validDateFromNow = validDate.get(type);
 
+            // 날짜를 년,월,일로 분리
             String[] tmp = strtDT.split("\\.");
             int year = Integer.parseInt(tmp[0]);
             int month = Integer.parseInt(tmp[1]);
 
             month += validDateFromNow;
+            // 약관기한을 더한 달이 13월을 넘긴 경우에만 +1년 이상 된 것이기 때문에 >
             if(month > MONTH_TOTAL_COUNT) {
                 year += month / MONTH_TOTAL_COUNT;
                 month %= MONTH_TOTAL_COUNT;
+                // 총 월이 12의 배수인 경우 년이 추가된 것이 아니기 때문에 그에 대한 처리 진행
                 if (month % MONTH_TOTAL_COUNT == 0) {
                     year--;
                     month = 12;
