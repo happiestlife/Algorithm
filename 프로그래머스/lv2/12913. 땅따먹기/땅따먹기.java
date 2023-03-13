2a905054244442b7295ab96d8f3b5995ab84e490
happiestlife <75258422+happiestlife@users.noreply.github.com>
2023-03-13 22:27:44 +0900

    [level 2] Title: 땅따먹기, Time: 92.65 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    
+    static int[][] dp;
+    
+    int solution(int[][] land) {
+        dp = new int[land.length][4];
+        for(int i = 0; i < dp.length; i++)
+            Arrays.fill(dp[i], -1);
+        
+        for (int i = 0; i < land[0].length; i++) {
+            dp(land, 0, i, i);
+        }
+
+        Arrays.sort(dp[0]);
+        return dp[0][3]; 
+    }
+    
+    public static int dp(int[][] land, int r, int c, int lastCol) {
+        if(r == land.length - 1){
+            return dp[r][c] = land[r][c];
+        }
+        // else if(dp[r][c] != 0){
+        //     return dp[r][c];
+        // }
+
+        int max = 0;
+        for (int i = 0; i < land[r].length; i++) {
+            if(i != lastCol){
+                if(dp[r+1][i] != -1)
+                    max = Math.max(dp[r+1][i], max);
+                else 
+                    max = Math.max(dp(land, r+1, i, i), max);
+            }
+        } 
+
+        return dp[r][c] = land[r][c] + max;
+    }
+}
